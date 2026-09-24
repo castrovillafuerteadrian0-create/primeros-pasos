9789ee5934e7129f809d3c3fc1f2c5e79c915212
Adrian <castrovillafuerteadrian0@gmail.com>
2026-09-24 10:36:48 +0200

    24/09

--- Ejercicios de refuerzo/src/ejercicio17.java
+++ Ejercicios de refuerzo/src/ejercicio17.java
@@ -5,6 +5,16 @@ public class ejercicio17 {
         int num1;
         int num2;
         Scanner input = new Scanner(System.in);
-        
+        System.out.println("Añade el valor de los dos numeros: ");
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+        if (num1 == num2){
+            System.out.println("Ambos numeros son iguales");
+        } else if (num1 > num2) {
+            System.out.println(num1 + " es mayor que " + num2);
+        }else {
+            System.out.println(num2 + " es mayor que " + num1);
+        }
+        input.close();
     }
 }
---
+++ Ejercicios de refuerzo/src/ejercicio18.java
@@ -0,0 +1,5 @@
+public class ejercicio18 {
+    public static void main(String[] args) {
+
+    }
+}
